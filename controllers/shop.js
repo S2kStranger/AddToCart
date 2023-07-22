d3ebf81e79e85b6df3f7d433885e2dc5fa89eca5
S2kStranger <s2kkasera@gmail.com>
2023-07-22 15:14:41 +0530

    cart created

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart=require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -14,9 +14,9 @@ exports.getProduct = (req,res,next) => {
   const prodId=req.params.productID;
   Product.findById(prodId,product => {
     res.render('shop/product-detail', {
-      product:product,
-      pageTitle:product.title,
-      path: '/products '
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
     });
   });
 }
@@ -37,6 +37,14 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+
+exports.postCart = (req,res,next) => {
+  const prodId=req.body.productId;
+  Product.findById(prodId,(product) => {
+    Cart.addProduct(prodId,product.price);
+  })
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
